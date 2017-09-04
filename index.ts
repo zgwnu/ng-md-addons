42450f68ddb7b028c14045f3e7134125451342ac
Onno Haldar <onno@zgw.nu>
2017-09-04 13:40:01 +0200

    refactor

--- index.ts
+++ index.ts
@@ -1,11 +1,1 @@
-export * from './datepicker/datepicker.module'
-
-// images viewer
-export * from './images-viewer/zgwnu-images-viewer.component'
-
-// toolbar
-export * from './toolbar/zgwnu-toolbar.component'
-export * from './toolbar/zgwnu-toolbar.service'
-
-// file input
-export * from './file-input/zgwnu-file-input.component'
+export * from './src'
