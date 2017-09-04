d30f10184890b999b3d3141b00534c4ac0d3de5e
Onno Haldar <onno@zgw.nu>
2017-09-04 15:56:02 +0200

    refactor

--- index.ts
+++ index.ts
@@ -1,1 +1,9 @@
-export * from './src'
+// images viewer
+export * from './images-viewer/zgwnu-images-viewer.component'
+
+// toolbar
+export * from './toolbar/zgwnu-toolbar.component'
+export * from './toolbar/zgwnu-toolbar.service'
+
+// file input
+export * from './file-input/zgwnu-file-input.component'
--- src/index.ts
+++
@@ -1,9 +0,0 @@
-// images viewer
-export * from './images-viewer/zgwnu-images-viewer.component'
-
-// toolbar
-export * from './toolbar/zgwnu-toolbar.component'
-export * from './toolbar/zgwnu-toolbar.service'
-
-// file input
-export * from './file-input/zgwnu-file-input.component'
